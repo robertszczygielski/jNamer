131f468f5c62238908529dc27f6609d81c4e50be
Robert Szczygielski <robertszczygielski@wp.eu>
2020-03-09 20:50:27 +0100

    Add possibility to generate more than one name #11

--- src/main/java/org/jnamer/NameGenerator.java
+++ src/main/java/org/jnamer/NameGenerator.java
@@ -1,5 +1,7 @@
 package org.jnamer;
 
+import java.util.List;
+
 public class NameGenerator {
 
     public static String generateOne() {
@@ -14,4 +16,15 @@ public class NameGenerator {
         return BasicNames.getRandomFemaleName();
     }
 
+    public static List<String> generateMulti(int numberOfNames) {
+        return BasicNames.getRandomNames(numberOfNames);
+    }
+
+    public static List<String> generateMultiMale(int numberOfNames) {
+        return BasicNames.getRandomMaleNames(numberOfNames);
+    }
+
+    public static List<String> generateMultiFemale(int numberOfNames) {
+        return BasicNames.getRandomFemaleNames(numberOfNames);
+    }
 }
