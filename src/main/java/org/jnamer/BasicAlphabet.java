277e7d01f2779c274f831ad9053af4ebc121d895
szczygr <Robert.Szczygielski@qiagen.com>
2020-06-25 13:57:53 +0200

    add big letters

--- src/main/java/org/jnamer/BasicAlphabet.java
+++ src/main/java/org/jnamer/BasicAlphabet.java
@@ -9,6 +9,10 @@ class BasicAlphabet {
             "a","b","c","d","e","f","g","h","i","j","k","l","m",
             "n","o","p","q","r","s","t","u","v","w","x","y","z");
 
+static final List<String> bigLetters = Arrays.asList(
+            "A","B","C","D","E","F","G","H","I","J","K","L","M",
+            "N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+
     static int maxIndex() {
         return letters.size() - 1;
     }
